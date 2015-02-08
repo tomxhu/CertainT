c0b7354d328e897582c3534c54c78dc4567db50f
Tommy Hu <tomxhu@gmail.com>
2015-02-07 20:08:42 -0500

    removed commented code

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -4,50 +4,6 @@ angular.module('App.controllers.Main', [
     'App.services.Prediction'
 ])
 
-    //.controller('MainController', function($scope, getCurrentPosition, getStation, getPrediction){
-    //    document.addEventListener('deviceready', function () {
-    //        $scope.watch = 0;
-    //
-    //        var int = 0;
-    //
-    //        function onSuccess(position) {
-    //            $scope.watch += 1;
-    //            $scope.lat = position.coords.latitude;
-    //            $scope.lon = position.coords.longitude;
-    //
-    //            getStation(position.coords.latitude, position.coords.longitude, function(data){
-    //                if (data.stop[0].distance < 100) {
-    //                    $scope.station = data.stop[0].parent_station_name;
-    //                    getPrediction(data.stop[0].parent_station, function(southBound, northBound){
-    //                        console.log(southBound);
-    //                        console.log(northBound);
-    //                        $scope.southBound = southBound;
-    //                        $scope.northBound = northBound;
-    //
-    //                        window.plugin.notification.local.add({
-    //                            id:         "notifications",  // A unique id of the notifiction
-    //                            message:    southBound.toString(),  // The message that is displayed
-    //                            title:      "test",  // The title of the message
-    //                        });
-    //                    });
-    //
-    //                }
-    //                console.log(data);
-    //
-    //            });
-    //
-    //        };
-    //        function onError(error) {
-    //            console.log('err');
-    //            alert('code: '    + error.code    + '\n' +
-    //            'message: ' + error.message + '\n');
-    //        }
-    //
-    //        var intervalID = window.setInterval(navigator.geolocation.getCurrentPosition, 10000);
-    //
-    //    });
-    //});
-
     .controller('MainController', function($scope, $interval, getCurrentPosition, getStation, getPrediction){
         document.addEventListener('deviceready', function () {
             $scope.watch = 0;
@@ -91,25 +47,6 @@ angular.module('App.controllers.Main', [
         });
     });
 
-
-
-
-//getCurrentPosition(function(position){
-//    $scope.lat = position.coords.latitude;
-//    $scope.lon = position.coords.longitude;
-//    getStation(position.coords.latitude, position.coords.longitude, function(data){
-//        console.log(data);
-//        $scope.station = data.stop[0].parent_station_name;
-//        getPrediction(data.stop[0].parent_station, function(southBound, northBound){
-//            console.log(southBound);
-//            console.log(northBound);
-//            $scope.southBound = southBound;
-//            $scope.northBound = northBound;
-//
-//        })
-//    });
-//});
-//
 
 
 
